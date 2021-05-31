4eb9adf4fc9c294048e424a2ebe03ff7770fe172
Gustav Höglund <Gustav.Hoglund@protonmail.com>
2021-05-31 10:48:06 +0200

    cleaned upp old files from deleted projects

--- Webbshop/webbshop/src/App.js
+++ Webbshop/webbshop/src/App.js
@@ -3,86 +3,27 @@ import Header from './Components/Header';
 import Container from './Components/Container';
 import {useState} from 'react';
 import axios from 'axios'
+import Data from "./articles.json"
 
 function App() {
   const [fetching, setFetching] = useState(false)
-  const [articles, setArticles] = useState([
-    {
-      id: 1,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 2,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 3,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 4,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 5,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 6,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    },
-    {
-      id: 7,
-      name: 'Samsung 55" UHDR Turbo Tv',
-      type: 'tv',
-      brand: 'Samsung',
-      price: 20000,
-      stock: 5,
-      description: 'En väldigt fin tv-apparat',
-    }
-  ]);
+  const [articles, setArticles] = useState();
 
   const fetchArticles = async (fetchURL) => {
-    try {
-        setFetching({fetching: true});
-        const response = await axios.get(fetchURL);
-        setArticles({articles: response.data});
-        setFetching({fetching: false})
-    } catch (e) {
-        console.log(e);
-        setFetching({fetching: false})
-    }
-};
+    setArticles({Data})
+  };
+
+  // const fetchArticles = async (fetchURL) => {
+  //   try {
+  //       setFetching({fetching: true});
+  //       const response = await axios.get(fetchURL);
+  //       setArticles({articles: response.data});
+  //       setFetching({fetching: false})
+  //   } catch (e) {
+  //       console.log(e);
+  //       setFetching({fetching: false})
+  //   }
+  // };
 
 
   return (
