ea82ebd86441cc84acdb949fb882509b7bc32ae3
Gustav Höglund <gustav.hoglund.syne20lin@tucsweden.com>
2021-06-16 20:08:15 +0200

    implemented pages for articles to be shown

--- Webbshop/webbshop/src/Components/Article.js
+++ Webbshop/webbshop/src/Components/Article.js
@@ -4,15 +4,18 @@ import PropTypes from 'prop-types';
 const Article = ({article}) => {
     return (
         <div className='article'>
-            <h3>{article.name}</h3>
-            <h2>{article.price}:-</h2>
+            <h4>{article.name}</h4>
+            {/* //börja här nedan fungerar ej. */}
+            <p id='stock' style={article.stock != 0 ? {color:green}:{color:red}}>{article.stock != 0 ? 'In Stock' : 'Out of Stock'}</p>
+            <h3>{article.price}:-</h3>
+            
         </div>
     )
 }
 
 
 Article.propTypes = {
-    id: PropTypes.number.isRequired,
+    id: PropTypes.number,
     name: PropTypes.string,
     type: PropTypes.string,
     brand: PropTypes.string,
--- Webbshop/webbshop/src/Components/ArticleWindow.js
+++ Webbshop/webbshop/src/Components/ArticleWindow.js
@@ -1,21 +1,77 @@
 import React from 'react';
-import Article from './Article'
+import Article from './Article';
+import {useState, useEffect} from 'react';
 
 
 const ArticleWindow = ({artilces}) => {
+
+    const [page, setPage] = useState(1);
+    const [articleList, setArticleList] = useState([]);
+
+    
+    const renderArticles = (articleList, page) =>{
+        let output = [];
+        switch(page)
+        {
+            case 1:
+                for (let index = 0; index < 12; index++) {
+                   output.push(articleList[index]);
+                    
+                }
+                
+                break;
+            case 2:
+                for (let index = 16; index < 32; index++) {
+                    output.push(articleList[index]);
+                     
+                 }
+                
+                break;
+            case 3:
+                for (let index = 32; index < 48; index++) {
+                    output.push(articleList[index]);
+                     
+                 }
+
+                break;
+            case 4:
+                for (let index = 48; index < 64; index++) {
+                    output.push(articleList[index]);
+                     
+                 }
+
+                break;
+            default:
+                for (let index = 0; index < 16; index++) {
+                    output.push(articleList[index]);
+                     
+                 }
+
+                break;
+        }
+        return output;
+    }
+    
+    useEffect(()=>{
+        setArticleList(
+            artilces.map((article)=>(
+            <Article 
+                key={article.id}
+                article={article}
+            />
+            )
+        ))
+
+    },[])
+
     return (
         <div className='articleWindow'>
-            {artilces.map((article)=>(
-                <Article 
-                    key={article.id}
-                    article={article}
-                />
-            )
-            
-            )}
+            {renderArticles(articleList, page)}
             
         </div>
     )
+
+    
 }
 
 
